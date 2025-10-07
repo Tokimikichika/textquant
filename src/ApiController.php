378e116d23141850b42a7b770154416f3ff5820b
Tokimikichika <ahileses@mail.ru>
2025-10-07 17:06:45 +0300

    feat: создания ApiContoller функции форматирующий в JSON

--- src/ApiController.php
+++ src/ApiController.php
@@ -5,6 +5,9 @@ namespace Tokimikichika\Find;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+/**
+ * Контроллер для API
+ */
 class ApiController
 {
     private TextAnalyzer $analyzer;
@@ -15,6 +18,18 @@ class ApiController
     }
 
 
+
+    /**
+     * Форматирует данные в JSON
+     */
+    private function json(Response $response, array $data, int $status): Response
+    {
+        $payload = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json; charset=utf-8')
+            ->withStatus($status);
+    }
 }
 
 
