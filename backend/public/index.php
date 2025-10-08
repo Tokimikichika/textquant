66e03fd8098bc992fc7abab4ac7b386fdf7472af
Tokimikichika <ahileses@mail.ru>
2025-10-08 16:17:55 +0300

    feat: упростил index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,29 +7,9 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Tokimikichika\Find\WordCounter;
-use Tokimikichika\Find\CharacterCounter;
-use Tokimikichika\Find\SentenceCounter;
-use Tokimikichika\Find\ParagraphCounter;
-use Tokimikichika\Find\TopWordAnalyzer;
-use Tokimikichika\Find\Service\TextAnalyzer;
-use Tokimikichika\Find\TextReader;
 use Tokimikichika\Find\Controller\ApiController;
 
 $app = AppFactory::create();
-
-$wordCounter = new WordCounter();
-$characterCounter = new CharacterCounter();
-$sentenceCounter = new SentenceCounter();
-$paragraphCounter = new ParagraphCounter();
-$topWordAnalyzer = new TopWordAnalyzer($wordCounter);
-$analyzer = new TextAnalyzer(
-	$wordCounter,
-	$characterCounter,
-	$sentenceCounter,
-	$paragraphCounter,
-	$topWordAnalyzer
-);
 
 $app->post('/api/v1/analyze/text', [new ApiController($analyzer), 'analyzeText']);
 
