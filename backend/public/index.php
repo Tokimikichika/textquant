d975274767a444f13a20502c06fbed747fa9e39a
Tokimikichika <ahileses@mail.ru>
2025-10-09 21:49:28 +0300

    feat: подключение контроллера по анализу URL в index

--- backend/public/index.php
+++ backend/public/index.php
@@ -10,13 +10,17 @@ require __DIR__ . '/../vendor/autoload.php';
 use Tokimikichika\Find\Controller\ApiController;
 use Tokimikichika\Find\Service\TextAnalyzer;
 use Tokimikichika\Find\Service\RandomTextService;
+use Tokimikichika\Find\Service\WebScraperService;
 
 $app = AppFactory::create();
 
 $analyzer = new TextAnalyzer();
 $randomTextService = new RandomTextService();
-$controller = new ApiController($analyzer, $randomTextService);
+$webScraperService = new WebScraperService();
+$controller = new ApiController($analyzer, $randomTextService, $webScraperService);
+
 $app->post('/api/v1/analyze/text', [$controller, 'analyzeText']);
+$app->post('/api/v1/analyze/url', [$controller, 'analyzeUrl']);
 $app->get('/api/v1/text/random', [$controller, 'randomText']);
 
 $app->run();
