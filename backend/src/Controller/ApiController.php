231def26edb7bbc99be1c6fdef280665d37b0aff
Tokimikichika <ahileses@mail.ru>
2025-10-09 18:58:42 +0300

    feat: настроил его в контроллере

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -5,11 +5,14 @@ namespace Tokimikichika\Find\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Tokimikichika\Find\Service\TextAnalyzer;
+use Tokimikichika\Find\Service\RandomTextService;
 
 class ApiController
 {
-    public function __construct(private TextAnalyzer $analyzer)
-    {
+    public function __construct(
+        private TextAnalyzer $analyzer,
+        private RandomTextService $randomTextService
+    ) {
     }
 
     /**
@@ -23,6 +26,20 @@ class ApiController
 
         return $response->withHeader('Content-Type', 'application/json; charset=utf-8');
     }
+
+    /**
+     * Возвращает сгенерированный текст из внешнего сервиса
+     */
+    public function randomText(Request $request, Response $response): Response
+    {
+        $text = $this->randomTextService->get();
+
+        $payload = json_encode(['text' => $text], JSON_UNESCAPED_UNICODE);
+        $response->getBody()->write($payload);
+
+        return $response->withHeader('Content-Type', 'application/json; charset=utf-8');
+    }
+
 }
 
 
