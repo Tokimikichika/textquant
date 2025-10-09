fd57fd02dc9b04e856ee29bfc55bc2335327db99
Tokimikichika <ahileses@mail.ru>
2025-10-09 21:58:39 +0300

    feat: подключение контроллеров в index

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,7 +7,9 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Tokimikichika\Find\Controller\ApiController;
+use Tokimikichika\Find\Controller\TextController;
+use Tokimikichika\Find\Controller\UrlController;
+use Tokimikichika\Find\Controller\RandomController;
 use Tokimikichika\Find\Service\TextAnalyzer;
 use Tokimikichika\Find\Service\RandomTextService;
 use Tokimikichika\Find\Service\WebScraperService;
@@ -17,10 +19,13 @@ $app = AppFactory::create();
 $analyzer = new TextAnalyzer();
 $randomTextService = new RandomTextService();
 $webScraperService = new WebScraperService();
-$controller = new ApiController($analyzer, $randomTextService, $webScraperService);
 
-$app->post('/api/v1/analyze/text', [$controller, 'analyzeText']);
-$app->post('/api/v1/analyze/url', [$controller, 'analyzeUrl']);
-$app->get('/api/v1/text/random', [$controller, 'randomText']);
+$textController = new TextController($analyzer);
+$urlController = new UrlController($analyzer, $webScraperService);
+$randomController = new RandomController($randomTextService);
+
+$app->post('/api/v1/analyze/text', [$textController, 'analyze']);
+$app->post('/api/v1/analyze/url', [$urlController, 'analyze']);
+$app->get('/api/v1/text/random', [$randomController, 'getText']);
 
 $app->run();
