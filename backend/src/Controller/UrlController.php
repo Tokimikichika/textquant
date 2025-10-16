df537c860aae090d1f08469641eacbf471216394
Tokimikichika <ahileses@mail.ru>
2025-10-16 16:22:53 +0300

    chore: добавил комментарии аннотация типов в контроллер UrlController.php

--- backend/src/Controller/UrlController.php
+++ backend/src/Controller/UrlController.php
@@ -6,15 +6,30 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Tokimikichika\Find\Service\UrlAnalysisService;
 
+/**
+ * Контроллер для анализа текста по URL.
+ *
+ * Отвечает за приём входных данных и делегирование
+ * в доменные сервисы. Исключения сервисов мапятся
+ * на HTTP-коды в JSON-ответах.
+ */
 class UrlController
 {
+    /**
+     * @param UrlAnalysisService $urlAnalysisService Сервис анализа содержимого страницы по URL
+     */
     public function __construct(
         private UrlAnalysisService $urlAnalysisService
     ) {
     }
 
     /**
-     * Анализирует содержимое веб-страницы по URL
+     * Анализирует содержимое веб-страницы по URL.
+     *
+     * @param Request  $request  HTTP-запрос (ожидается JSON с ключом `url`)
+     * @param Response $response HTTP-ответ
+     *
+     * @return Response JSON-ответ с результатами анализа или сообщением об ошибке
      */
     public function analyze(Request $request, Response $response): Response
     {
@@ -37,7 +52,13 @@ class UrlController
     }
 
     /**
-     * Форматирует ответ в JSON
+     * Форматирует ответ в JSON.
+     *
+     * @param Response $response Базовый HTTP-ответ
+     * @param array    $data     Данные для сериализации в JSON
+     * @param int      $status   HTTP-статус ответа
+     *
+     * @return Response Ответ с заголовком `application/json; charset=utf-8`
      */
     private function json(Response $response, array $data, int $status = 200): Response
     {
