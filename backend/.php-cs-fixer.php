a844c2a8a546b718c8dab97ef0ee62e48e85f2be
Tokimikichika <ahileses@mail.ru>
2025-10-17 10:08:57 +0300

    chore: настройка фиксера

--- backend/.php-cs-fixer.php
+++ backend/.php-cs-fixer.php
@@ -3,15 +3,23 @@
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
     ->exclude('vendor')
-    ->exclude('tests')
     ->name('*.php');
 
-$config = new PhpCsFixer\Config();
-return $config
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
+        '@PHP80Migration' => true,
+        '@PHP80Migration:risky' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
+        'declare_strict_types' => false,
+        'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
+        'concat_space' => ['spacing' => 'one'],
+        'single_quote' => true,
+        'native_function_invocation' => false,
+        'fully_qualified_strict_types' => false,
+        'blank_line_before_statement' => ['statements' => ['return']],
     ])
     ->setFinder($finder);
