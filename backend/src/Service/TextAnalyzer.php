0c9d52c76f566337363be9916b750cbff1367a45
Tokimikichika <ahileses@mail.ru>
2025-10-18 09:10:03 +0300

    chome: применил проверку стилей

--- backend/src/Service/TextAnalyzer.php
+++ backend/src/Service/TextAnalyzer.php
@@ -16,14 +16,14 @@ class TextAnalyzer extends ExternalTextAnalyzer
     public function analyze(string $text, string $source = 'text'): array
     {
         return [
-            'source'     => $source,
-            'words'      => $this->countWords($text),
-            'characters' => $this->countCharacters($text, true),
-            'sentences'  => $this->countSentences($text),
-            'paragraphs' => $this->countParagraphs($text),
-            'avg_word_length'    => $this->calculateAverageWordLength($text),
+            'source'              => $source,
+            'words'               => $this->countWords($text),
+            'characters'          => $this->countCharacters($text, true),
+            'sentences'           => $this->countSentences($text),
+            'paragraphs'          => $this->countParagraphs($text),
+            'avg_word_length'     => $this->calculateAverageWordLength($text),
             'avg_sentence_length' => $this->calculateAverageSentenceLength($text),
-            'top_words'          => $this->formatTopWords($this->getTopWords($text)),
+            'top_words'           => $this->formatTopWords($this->getTopWords($text)),
         ];
     }
 }
