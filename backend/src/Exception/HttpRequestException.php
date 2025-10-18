5da5fd3ad0dafc9bfe6dc7a806917651e9c530ec
Tokimikichika <ahileses@mail.ru>
2025-10-18 13:07:18 +0300

    chome: перенос текст сообщения в exception - HttpRequestException.php

--- backend/src/Exception/HttpRequestException.php
+++ backend/src/Exception/HttpRequestException.php
@@ -7,4 +7,15 @@ namespace Tokimikichika\Find\Exception;
  */
 class HttpRequestException extends AppException
 {
+    public function __construct(string $url = '', string $error = '', int $code = 0, ?\Throwable $previous = null)
+    {
+        $message = 'Ошибка HTTP-запроса';
+        if ($url) {
+            $message .= " к {$url}";
+        }
+        if ($error) {
+            $message .= ": {$error}";
+        }
+        parent::__construct($message, $code, $previous);
+    }
 }
